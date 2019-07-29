200da4fd7e3c6a807e0b25ef4a5823875db47b8f
yassir ramdani <yassir.ramdani@epita.fr>
2019-07-29 05:23:21 +0200

    StoriesView implemented

--- InstaUI/InstaUI/ContentView.swift
+++ InstaUI/InstaUI/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             List {
+                StoriesView()
                 PostCell()
             }.navigationBarTitle("InstaUI", displayMode: .inline)
         }
@@ -60,6 +61,79 @@ struct PostCell: View {
                             .lineLimit(4)
                             .font(Font.system(size: 12.5))
                             .foregroundColor(.init(white: 0.2))
+        }
+    }
+}
+
+struct StoriesView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            
+            // Stories text + Watch all
+            HStack {
+               Text("Stories")
+               Spacer()
+               Image("Watch")
+               Text("Watch all")
+            }
+            
+            // Stories Circles
+            ScrollView([.horizontal], showsIndicators: false){
+                HStack {
+                    VStack {
+                        ZStack(alignment: .bottomTrailing) {
+                            Image("AvatarBig")
+                            Image("Add")
+                        }
+                        Text("Your Story")
+                            .font(Font.system(size: 13.5))
+                    }.padding(.trailing, 12)
+                    
+                    
+                    VStack {
+                        ZStack {
+                            Image("Border")
+                            Image("AvatarBig1")
+                        }
+                        Text("beardguy")
+                            .font(Font.system(size: 13.5))
+                    }.padding(.trailing, 12)
+                    
+                    
+                    VStack {
+                        ZStack {
+                            Image("Border")
+                            Image("AvatarBig2")
+                        }
+                        Text("elleranas")
+                            .font(Font.system(size: 13.5))
+                    }.padding(.trailing, 12)
+                   
+                    
+                    VStack {
+                        ZStack {
+                            Image("Border")
+                            Image("AvatarBig3")
+                        }
+                        Text("Ally")
+                            .font(Font.system(size: 13.5))
+                    }.padding(.trailing, 12)
+                    
+                    VStack {
+                        ZStack {
+                            Image("Border")
+                            Image("AvatarBig4")
+                        }
+                        Text("grandpa")
+                            .font(Font.system(size: 13.5))
+                    }.padding(.trailing, 12)
+                    
+                    
+                    
+                }.padding(.leading, 16)
+            }.padding(.leading, -20)
+             .padding(.trailing, -20)
+
         }
     }
 }
